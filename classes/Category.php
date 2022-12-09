fb49df7da3b626f078f52794fb03b2d005d42715
MOHAMMED BOUABDELLAOUI <118568976+SimoWoold@users.noreply.github.com>
2022-12-09 22:09:55 +0100

    Add verification of the user's presence in the database

---
+++ classes/Category.php
@@ -0,0 +1,26 @@
+<?php
+include 'Connection.php';
+class Ctegory{
+    
+}
+
+$email = "mohammed@kclkjhx.com";
+if (!preg_match("/^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $email)){
+    echo 'check your emali';
+}
+$nim = "0855555555";
+$nom = "Mohammed";
+trim($nom);
+htmlspecialchars($email);
+if(preg_match('/^([a-zA-Z]+$)/',$nom)){
+
+    print 'chen valede<br>';
+}else{
+    print 'forma chine invalede';
+}
+if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+echo 'forma de emaile invalede';
+}
+if(!preg_match('/^0([67])([0-9]{8})/',$nim)){
+    echo 'numero invaled';
+}
